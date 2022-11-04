d7f44ca9becac54c84eea728f9aecace75518377
EunmiHwang <cncn6666@naver.com>
2022-11-04 15:04:50 +0900

    [feature] 실패시 호출되는 ApiResponse 데이터 클래스 생성하기 (#1)

--- network/src/main/java/com/kakaovx/practice/network/ApiResponse.kt
+++ network/src/main/java/com/kakaovx/practice/network/ApiResponse.kt
@@ -2,6 +2,7 @@ package com.kakaovx.practice.network
 
 import com.kakaovx.practice.network.exceptions.NoContentException
 import okhttp3.Headers
+import okhttp3.ResponseBody
 import retrofit2.Response
 
 sealed class ApiResponse<out T> {
@@ -10,6 +11,14 @@ sealed class ApiResponse<out T> {
         val headers: Headers = response.headers()
         val data: T by lazy { response.body() ?: throw NoContentException(statusCode.code) }
         override fun toString(): String = "[ApiResponse.Success](data=$data)"
+    }
+
+    data class Failure<T>(val response: Response<T>) : ApiResponse<T>() {
+        val statusCode: StatusCode = getStatusCodeFromResponse(response)
+        val headers: Headers = response.headers()
+        val errorBody: ResponseBody? = response.errorBody()
+        override fun toString(): String = errorBody?.toString()
+            ?: "[ApiResponse.Failure.Error-$statusCode](errorResponse=$response)"
     }
 
     fun <T> getStatusCodeFromResponse(response: Response<T>): StatusCode {
