3817670662dbabfe5e24b222305b307423b7ecab
EunmiHwang <cncn6666@naver.com>
2022-11-07 17:42:07 +0900

    [feature] Hilt 의존성 주입하기 (#1)

--- buildSrc/src/main/java/com/kakaovx/practice/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/kakaovx/practice/buildsrc/Dependencies.kt
@@ -1,10 +1,15 @@
 package com.kakaovx.practice.buildsrc
 
 object Versions {
+    internal const val HILT = "2.44"
     internal const val RETROFIT = "2.9.0"
 }
 
 object Dependencies {
+    const val hilt = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val hiltCompiler = "com.google.dagger:hilt-compiler:${Versions.HILT}"
+    const val hiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT}"
+
     const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
     const val retrofitConverterGson = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
     const val retrofitConverterScalars = "com.squareup.retrofit2:converter-scalars:${Versions.RETROFIT}"
