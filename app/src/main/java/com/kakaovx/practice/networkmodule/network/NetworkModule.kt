a29233563e9788a048a5c2123f15fa45c44e0d81
EunmiHwang <cncn6666@naver.com>
2022-11-08 11:03:48 +0900

    [feature] Network 관련 모듈 추가하기 (#1)

---
+++ app/src/main/java/com/kakaovx/practice/networkmodule/network/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.kakaovx.practice.networkmodule.network
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                setLevel(HttpLoggingInterceptor.Level.BASIC)
+            })
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+}
