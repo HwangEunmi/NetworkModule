127dbca292a1e71cac026781e9dead9214e6e752
EunmiHwang <cncn6666@naver.com>
2022-11-28 14:49:50 +0900

    [feature] 통신 오류 타입 목록 클래스 추가하기 (#1)

---
+++ network/src/main/java/com/kakaovx/practice/network/constant/ApiErrorType.kt
@@ -0,0 +1,12 @@
+package com.kakaovx.practice.network.constant
+
+/**
+ * @author Jinny
+ * 통신 오류 타입 목록 클래스
+ * [TYPE_HTTP_ERROR]은 HTTP 통신 에러다.
+ * [TYPE_SERVER_ERROR]은 API 통신 에러다.
+ */
+enum class ApiErrorType {
+    TYPE_HTTP_ERROR,
+    TYPE_SERVER_ERROR
+}
