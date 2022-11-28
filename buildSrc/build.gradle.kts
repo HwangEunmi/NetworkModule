908f750ebb58b754fd85757d9de3f745c96053ea
EunmiHwang <cncn6666@naver.com>
2022-11-28 09:43:51 +0900

    [feature] gradle build task시 detekt 자동으로 실행되지 않도록 script문 추가하기 (#1)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,10 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+// gradle의 check task 실행시 detekt 수행 X
+tasks.named("check").configure {
+    this.setDependsOn(this.dependsOn.filterNot {
+        it is TaskProvider <*> && it.name == "detekt"
+    })
+}
