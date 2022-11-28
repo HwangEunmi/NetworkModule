85d9306a9c44b6e62ec5210c774351614c694bec
EunmiHwang <cncn6666@naver.com>
2022-11-28 14:49:17 +0900

    [feature] Exception 상태코드 목록 클래스 추가하기 (#1)

---
+++ network/src/main/java/com/kakaovx/practice/network/constant/ExceptionStatusCode.kt
@@ -0,0 +1,9 @@
+package com.kakaovx.practice.network.constant
+
+/**
+ * @author Jinny
+ * Exception 상태 코드 목록 클래스
+ */
+enum class ExceptionStatusCode(val code: String) : StatusCode {
+    CoroutineException("CoroutineException")
+}
