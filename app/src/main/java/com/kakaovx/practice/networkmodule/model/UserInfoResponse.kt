4e6beae943881b6a55c47a5a647dbfda57bc2bb9
EunmiHwang <cncn6666@naver.com>
2022-11-28 15:44:49 +0900

    [feature] API Response 추가하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/model/UserInfoResponse.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/model/UserInfoResponse.kt
@@ -1,7 +1,25 @@
 package com.kakaovx.practice.networkmodule.model
 
+import com.kakaovx.practice.networkmodule.network.model.IServerResponse
+import com.kakaovx.practice.networkmodule.network.model.ITestServerResponse
+
 data class UserInfoResponse(
-    val id: Int,
+// val code: String = "E000", // 테스트용 (성공이라고 가정)
+    override val code: String,
+    override val msg: String,
+    override val data: UserInfo
+
+) : IServerResponse<UserInfoResponse.UserInfo> {
+    data class UserInfo(
+        val id: Int,
+        val login: String,
+        val avatar_url: String
+    )
+}
+
+// Git API 테스트용 데이터
+data class TestUserInfoResponse(
     val login: String,
+    override val id: Int,
     val avatar_url: String
-)
+): ITestServerResponse
