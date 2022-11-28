beacd27df848ca97853c19014c17e8370e8ab24d
EunmiHwang <cncn6666@naver.com>
2022-11-28 14:49:07 +0900

    [feature] 네트워크 응답 상태코드 목록 클래스 추가하기 (#1)

---
+++ network/src/main/java/com/kakaovx/practice/network/constant/NetworkStatusCode.kt
@@ -0,0 +1,9 @@
+package com.kakaovx.practice.network.constant
+
+/**
+ * @author Jinny
+ * 네트워크 응답 상태 코드 목록 클래스
+ */
+enum class NetworkStatusCode(val code: String) : StatusCode {
+    NetworkDisconnect("NetworkDisconnect")
+}
