6926691cc5b9475250c62fe9850a50cb4e29410b
EunmiHwang <cncn6666@naver.com>
2022-11-28 15:42:06 +0900

    [feature] Git API Repository/Impl 추가하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/repository/GitRepository.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/repository/GitRepository.kt
@@ -1,10 +1,14 @@
 package com.kakaovx.practice.networkmodule.repository
 
-import com.kakaovx.practice.network.ApiResponse
+import com.kakaovx.practice.networkmodule.model.TestUserInfoResponse
 import com.kakaovx.practice.networkmodule.model.UserInfoResponse
+import com.kakaovx.practice.networkmodule.network.ServerApiResponse
+import com.kakaovx.practice.networkmodule.network.TestServerApiResponse
 
 interface GitRepository {
-    suspend fun getUserInfo(username: String): ApiResponse<UserInfoResponse>
+    // suspend fun getUserInfo(username: String): ServerApiResponse<UserInfoResponse>
+    suspend fun getUserInfo(username: String): TestServerApiResponse<TestUserInfoResponse>
 
-    suspend fun getUserList(): ApiResponse<List<UserInfoResponse>>
+    // suspend fun getUserList(): ServerApiResponse<List<UserInfoResponse>>
+    suspend fun getUserList(): TestServerApiResponse<List<TestUserInfoResponse>>
 }
--- app/src/main/java/com/kakaovx/practice/networkmodule/repository/GitRepositoryImpl.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/repository/GitRepositoryImpl.kt
@@ -1,11 +1,23 @@
 package com.kakaovx.practice.networkmodule.repository
 
-import com.kakaovx.practice.networkmodule.network.GithubApi
+import com.kakaovx.practice.networkmodule.datasource.GitDataSource
+import com.kakaovx.practice.networkmodule.model.TestUserInfoResponse
+import com.kakaovx.practice.networkmodule.network.ServerApiResponse
+import com.kakaovx.practice.networkmodule.network.TestServerApiResponse
+import javax.inject.Inject
 
-class GitRepositoryImpl(
-    private val githubApi: GithubApi
+class GitRepositoryImpl @Inject constructor(
+    private val gitDataSource: GitDataSource
 ) : GitRepository {
-    override suspend fun getUserInfo(username: String) = githubApi.getUserInfo(username)
+    override suspend fun getUserInfo(username: String): TestServerApiResponse<TestUserInfoResponse> {
+        val result = gitDataSource.getUserInfo(username)
+        // return ServerApiResponse.of { result }
+        return TestServerApiResponse.of { result }
+    }
 
-    override suspend fun getUserList() = githubApi.getUserList()
+    override suspend fun getUserList(): TestServerApiResponse<List<TestUserInfoResponse>> {
+        val result = gitDataSource.getUserList()
+        // return ServerApiResponse.of { result }
+        return TestServerApiResponse.of { result }
+    }
 }
