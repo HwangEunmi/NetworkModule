86ed6cf304aa11acb597e5b9ec14ba44ea35734a
EunmiHwang <cncn6666@naver.com>
2022-12-05 11:38:28 +0900

    [feature] callback 네이밍 수정하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/util/Utils.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/util/Utils.kt
@@ -1,9 +1,9 @@
 package com.kakaovx.practice.networkmodule.util
 
-suspend fun <T : Any, R : Any> whenAllNotNull(vararg options: T?, block: suspend (List<T>) -> R, notBlock: suspend () -> Unit) {
+suspend fun <T : Any, R : Any> whenAllNotNull(vararg options: T?, notNullBlock: suspend (List<T>) -> R, nullBlock: suspend () -> Unit) {
     if (options.all { it != null }) {
-        block(options.filterNotNull())
+        notNullBlock(options.filterNotNull())
     } else {
-        notBlock()
+        nullBlock()
     }
 }
