c9eed139af714b4dbffd9233ecf74f12a740c248
EunmiHwang <cncn6666@naver.com>
2022-12-07 14:37:49 +0900

    [feature] Application에 Context 프로퍼티 추가하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/App.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/App.kt
@@ -4,4 +4,15 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+
+    companion object {
+        lateinit var appContext: Application
+            private set
+    }
+
+}
