8643d946508839a4443bef49b4ce9d488d17db77
EunmiHwang <cncn6666@naver.com>
2023-01-25 11:08:02 +0900

    [feature] LoadingHandleCallback 추가하기 (#1)

---
+++ app/src/main/java/com/kakaovx/practice/networkmodule/network/NetworkUnhandledContent.kt
@@ -0,0 +1,5 @@
+package com.kakaovx.practice.networkmodule.network
+
+import com.kakaovx.practice.networkmodule.ui.constant.LoadingState
+
+typealias LoadingHandleCallback = (LoadingState.Loading) -> Unit
