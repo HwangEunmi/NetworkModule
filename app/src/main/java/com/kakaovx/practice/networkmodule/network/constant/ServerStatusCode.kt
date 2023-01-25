53e8ea84693c8c3c9d99fbaa26a3e5c3b8a92bd0
EunmiHwang <cncn6666@naver.com>
2023-01-25 10:57:08 +0900

    [feature] API 응답 상태 코드 목록 클래스에 토큰 만료 코드 추가하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/network/constant/ServerStatusCode.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/network/constant/ServerStatusCode.kt
@@ -1,14 +1,13 @@
 package com.kakaovx.practice.networkmodule.network.constant
 
-import com.kakaovx.practice.network.constant.StatusCode
-
 /**
  * @author Jinny
  * API 응답 상태 코드 목록 클래스
  */
-enum class ServerStatusCode(val code: String) : StatusCode {
+enum class ServerStatusCode(val code: String) {
     Success("E000"),
 
     HttpError(""), // 이 값을 받으면 '일시적인 오류'팝업 등 하나로 통일 (통신 오류니까)
+    TokenExpiration("EEEE"), // 토큰 만료 코드
     ResponseNotBaseResponse("XXXX") // Response가 BaseResponse를 상속받지 않은 경우
 }
