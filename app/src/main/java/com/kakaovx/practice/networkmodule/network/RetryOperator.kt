8ddec73430ca8c41540499ee00a502561e8cf929
EunmiHwang <cncn6666@naver.com>
2023-01-31 14:53:55 +0900

    [feature] RetryOperator 제거하기 (#1)

--- app/src/main/java/com/kakaovx/practice/networkmodule/network/RetryOperator.kt
+++ app/src/main/java/com/kakaovx/practice/networkmodule/network/RetryOperator.kt
@@ -1,17 +1,7 @@
 package com.kakaovx.practice.networkmodule.network
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.onEach
-
-// TODO : 재시도 구현해야되는데 이렇게 하기 보다는 StateFlow collect 다시 호출하는 방법 찾아보기 (flow를 copy하던지 해서)
-internal inline fun <T> retryOperator(
-    retryTrigger: RetryTrigger,
-    crossinline flowProvider: () -> Flow<T>
-) =
-    retryTrigger.retryFlow
 
 // internal inline fun <T> retryOperator(
 //     retryTrigger: RetryTrigger,
@@ -21,13 +11,11 @@ internal inline fun <T> retryOperator(
 //         .flatMapConcat { flowProvider() }
 //         .onEach { retryTrigger.retryFlow.value = RetryTrigger.State.IDLE }
 
-class RetryTrigger {
-    enum class State { RETRY, IDLE }
-
-    val retryFlow = MutableStateFlow(State.RETRY)
-
-    fun retry() {
-        retryFlow.value = State.RETRY
-    }
-}
-
+// internal inline fun <T> retryOperator(
+//     retryTrigger: RetryTrigger,
+//     crossinline flowProvider: () -> Flow<T>
+// )
+//
+// class RetryTrigger {
+//
+// }
